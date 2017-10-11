322384e442400507b40d1bf600de6a9acbe6baf2
Michael Pye <mail@mdpye.co.uk>
2017-10-11 12:01:06 +0100

    Hybrid layout

---
+++ keyboards/satan/keymaps/mp-ansi/keymap.c
@@ -0,0 +1,38 @@
+// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
+// this is the style you want to emulate.
+
+#include "satan.h"
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _DEF 0
+#define _FN  1
+#define _MEDIA 2
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_DEF] = KEYMAP_ISO_SPLITRSHIFT(
+            KC_ESC,  KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,    KC_0,    KC_MINS, KC_EQL,     KC_BSPC, \
+            KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,    KC_LBRC, KC_RBRC,    KC_NUHS, \
+            MO(_FN), KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT, KC_NO,      KC_ENT, \
+            KC_LSFT, KC_NUBS, KC_Z,    KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,    KC_NO, \
+            KC_LCTL, KC_LGUI, KC_LALT,             KC_SPACE,                        KC_RALT, MO(_FN) ,MO(_MEDIA), KC_RCTL),
+
+    [_FN] = KEYMAP_ISO_SPLITRSHIFT(
+            KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,         KC_F8,   KC_F9,   KC_F10,        KC_F11,  KC_F12,  KC_DEL, \
+            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_HOME, KC_PGDN,       KC_PGUP, KC_END,  KC_NO,         KC_NO,   KC_NO,   KC_NO,   \
+            KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_BSPC, KC_NO,   KC_LEFT, KC_DOWN,       KC_UP,   KC_RGHT, KC_NO,         KC_NO,   KC_NO,   KC_NO,   \
+            KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   LCTL(KC_LEFT), KC_PGDN, KC_PGUP, LCTL(KC_RGHT), KC_NO,   KC_NO,   KC_NO, \
+            KC_TRNS, KC_TRNS, KC_TRNS,             KC_ENT,                                                  KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS),
+
+    [_MEDIA] = KEYMAP_ISO_SPLITRSHIFT(
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, \
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, \
+            KC_TRNS, KC_TRNS, KC_TRNS,             KC_MUTE,                                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+};
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
+}
