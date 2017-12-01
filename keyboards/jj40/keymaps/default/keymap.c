9a77b73a04b8e2218052591969566f16faf9ec3d
Michael Pye <mail@mdpye.co.uk>
2017-12-01 11:22:08 +0000

    improvements through usage

--- keyboards/jj40/keymaps/default/keymap.c
+++ keyboards/jj40/keymaps/default/keymap.c
@@ -21,14 +21,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  Fn  | Alt  | GUI  |Lower |    Space    |Raise | Alt  | GUI  | Ctrl |Enter |
+ * | Ctrl |  Fn  | GUI  | LAlt |Lower |    Space    |Raise | GUI  | RAlt | Ctrl |Enter |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,    KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,      KC_G,    KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,    KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_LCTL, MO(_FN), KC_LGUI, KC_LALT, MO(_LOWER),KC_SPC,  KC_SPC,  MO(_RAISE),KC_RALT, KC_RGUI, KC_RCTL, KC_ENT \
+  KC_LCTL, MO(_FN), KC_LGUI, KC_LALT, MO(_LOWER),KC_SPC,  KC_SPC,  MO(_RAISE),KC_RGUI, KC_RALT, KC_RCTL, KC_ENT \
 ),
 
 /* Raise
@@ -37,15 +37,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | #    |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   \  |      |      |      |      |      |   -  |   =  |    [ |   ]  |Shift |
+ * | Shift|   \  |      |      |      |      |      |   -  |   [  |    ] |   =  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |    Enter    |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = KEYMAP( \
   KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-  XXXXXXX,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NONUS_HASH, \
-  _______, KC_NUBS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,\
+  XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NONUS_HASH, \
+  _______, KC_NUBS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_LBRC, KC_RBRC, KC_EQL,  _______,\
   _______, _______, _______, _______, _______, KC_ENT,  KC_ENT,  _______, _______, _______, _______, _______ \
 ),
 
