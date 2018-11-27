f621c3a8fe100b491b35acaba2fe2010e2d44e61
Michael Pye <mail@mdpye.co.uk>
2018-11-27 18:58:03 +0000

    Define KEYMAP

--- keyboards/jj40/keymaps/default/keymap.c
+++ keyboards/jj40/keymaps/default/keymap.c
@@ -12,6 +12,8 @@ enum custom_keycodes {
   RAISE,
 };
 
+#define KEYMAP LAYOUT_ortho_4x12
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
