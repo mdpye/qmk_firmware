9bb3a26a22c97fc1f60c15b71b1b00f39f6cd0c6
Michael Pye <mail@mdpye.co.uk>
2018-11-27 16:39:27 +0000

    Tristate cases lockups

--- keyboards/jj40/keymaps/default/keymap.c
+++ keyboards/jj40/keymaps/default/keymap.c
@@ -100,10 +100,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 };
 
-uint32_t layer_state_set_user(uint32_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _BRACKETS);
-}
-
 // Loop
 void matrix_scan_user(void) {
   // Empty
