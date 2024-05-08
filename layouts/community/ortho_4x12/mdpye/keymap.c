9774da1680f9c25e943b61f4c06b2eb6ccb52087
Michael Pye <mail@mdpye.co.uk>
2024-05-08 09:10:08 +0100

    reorder mac modifier row

--- layouts/community/ortho_4x12/mdpye/keymap.c
+++ layouts/community/ortho_4x12/mdpye/keymap.c
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_Q, KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_TAB,  KC_A, KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_LCTL, KC_LGUI, FNS,  KC_LALT, LOW_M, KC_SPC, KC_ENT, RAI_M, KC_RGUI, KC_RALT, FNS,     KC_RCTL \
+  KC_LCTL, FNS,  KC_LALT, KC_LGUI, LOW_M, KC_SPC, KC_ENT, RAI_M, KC_RGUI, KC_RALT, FNS,     KC_RCTL \
 ),
 
 /* Raise
@@ -169,9 +169,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER_MAC] = LAYOUT_ortho_4x12( \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MA_HOME, MA_PGDN, MA_PGUP, MA_END,  XXXXXXX, KC_DEL, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MA_HOME, KC_PGDN, KC_PGUP, MA_END,  XXXXXXX, KC_DEL, \
   LINUX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MA_WDLF, MA_PGDN, MA_PGUP, MA_WDRT, XXXXXXX, _______, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MA_WDLF, KC_PGDN, KC_PGUP, MA_WDRT, XXXXXXX, _______, \
   _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______ \
 ),
 
